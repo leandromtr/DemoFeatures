757ea7bdbd262b29347015ff7d9d21ec57909070
leandromtr <leandromtr@hotmail.com>
2021-04-08 19:27:43 +0100

    Record considerations

--- RecordDemo/Program.cs
+++ RecordDemo/Program.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 
+/*
+ *  Benefits of Records:
+ *  - Simple to setup
+ *  - Thread to set up
+ *  - Easy/ safe to share
+ *  
+ *  When to use Records:
+ *  - Capturing external data that doesn't change 
+ *  - API calls
+ *  - Processing data
+ *  - Read-only data
+ *  
+ *  When not to use Records:
+ *  - When is necessary to change the data (Entity Framework)
+ *  
+ */
+
+
 namespace RecordDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Record1 r1a = new (FirstName: "Leandro", LastName: "Reis");
-            Record1 r1b = new (FirstName: "Leandro", LastName: "Reis");
-            Record1 r1c = new (FirstName: "Selin", LastName: "Civanbay");
+            Record1 r1a = new(FirstName: "Leandro", LastName: "Reis");
+            Record1 r1b = new(FirstName: "Leandro", LastName: "Reis");
+            Record1 r1c = new(FirstName: "Selin", LastName: "Civanbay");
 
-            Class1 c1a = new (FirstName: "Ororo", LastName: "Munroe");
-            Class1 c1b = new (FirstName: "Ororo", LastName: "Munroe");
-            Class1 c1c = new (FirstName: "Susan", LastName: "Storm");
+            Class1 c1a = new(FirstName: "Ororo", LastName: "Munroe");
+            Class1 c1b = new(FirstName: "Ororo", LastName: "Munroe");
+            Class1 c1c = new(FirstName: "Susan", LastName: "Storm");
 
             Console.WriteLine("Record Type");
             Console.WriteLine($"To String: { r1a}");
@@ -38,7 +56,7 @@ namespace RecordDemo
             Console.WriteLine("************************************* ");
             Console.WriteLine("Class Type");
             Console.WriteLine($"To String: { c1a}");
-            Console.WriteLine($"Are the two object equal: { Equals (c1a, c1b) }");
+            Console.WriteLine($"Are the two object equal: { Equals(c1a, c1b) }");
             Console.WriteLine($"Are the two object reference equal: { ReferenceEquals(c1a, c1b) }");
             Console.WriteLine($"Are the two object ==: { c1a == c1b }");
             Console.WriteLine($"Are the two object !=: { c1a != c1c }");
@@ -49,7 +67,7 @@ namespace RecordDemo
 
             Console.WriteLine();
             Console.WriteLine("************************************* ");
-            Record2 r2a = new(FirstName: "Leandro", LastName: "Reis", Adress:"123, Street");
+            Record2 r2a = new(FirstName: "Leandro", LastName: "Reis", Address: "123, Street");
             Console.WriteLine($"r2a value {r2a}");
             Console.WriteLine($"r2a fn: {r2a.FirstName} ln: { r2a.LastName}");
             Console.WriteLine(r2a.SayTheAddress());
@@ -59,7 +77,10 @@ namespace RecordDemo
         // A record is just a fancy read-only class
         public record Record1(string FirstName, string LastName);
 
-        public record Record2(string FirstName, string LastName, string Adress)
+        // class can inherit from a Record, but not from another class in this context 
+        public record User1(int Id, string FirstName, string LastName) : Record1(FirstName, LastName);
+
+        public record Record2(string FirstName, string LastName, string Address)
         {
             private string _firstName = FirstName;
             public string FirstName
@@ -68,11 +89,11 @@ namespace RecordDemo
                 init { }
             }
 
-            internal string Adress { get; init; } = Adress;   
+            internal string Address { get; init; } = Address;   
             public string FullName { get => $"{ FirstName} { LastName} "; }
             public string SayTheAddress()
             {
-                return $"Your Adress is {Adress}";
+                return $"Your Address is {Address}";
             }
         }
 
