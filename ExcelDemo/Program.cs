1f30dc9ef45a76d5fbd8e4514369094023f51969
leandro <leandromtr@hotmail.com>
2021-04-12 21:33:12 +0100

    Save excel file with list as a content

--- ExcelDemo/Program.cs
+++ ExcelDemo/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ExcelDemo
 {
-    class Program
+    partial class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var file = new FileInfo(@"C:\Demo\ExcelFile.xlsx");
 
+            var people = GetSetupData();
+
+            await SaveExcelFile(people, file);
+
+        }
+
+        private static async Task SaveExcelFile(List<PersonModel> people, FileInfo file)
+        {
+            DeleteIfExists(file);
+
+            using var package = new ExcelPackage(file);
+            var workSheet = package.Workbook.Worksheets.Add("MainReport");
+
+            var range = workSheet.Cells["A1"].LoadFromCollection(people, true);
+            range.AutoFitColumns();
+
+            await package.SaveAsync();
+        }
+
+        private static void DeleteIfExists(FileInfo file)
+        {
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
+
+        private static List<PersonModel> GetSetupData()
+        {
+            List<PersonModel> output = new()
+            {
+                new() { Id = 1, FistName = "Leandro", LastName = "Reis" },
+                new() { Id = 2, FistName = "Lucas", LastName = "Souza" },
+                new() { Id = 3, FistName = "Teteus", LastName = "Silva" }
+            };
+            return output;
         }
     }
 }
