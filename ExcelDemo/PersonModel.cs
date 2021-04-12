64bbfec5d4c8b1370fa002e609a408dc27f8225d
leandromtr <leandromtr@hotmail.com>
2021-04-12 21:33:12 +0100

    Save excel file with list as a content

---
+++ ExcelDemo/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ExcelDemo
+{
+    partial class Program
+    {
+        public class PersonModel
+        {
+            public int Id { get; set; }
+            public string FistName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
