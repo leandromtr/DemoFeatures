0db66d8ce38e08dcd3ee1afffc49f9a652acced3
leandro <leandromtr@hotmail.com>
2021-04-13 21:05:18 +0100

    Update/Insert and delete record in mongo db

--- MongoDbDemo/Program.cs
+++ MongoDbDemo/Program.cs
@@ -31,24 +31,46 @@ namespace MongoDbDemo
 
 
             // Return
-            var recs = db.LoadResource<PersonModel>("Users");
-            foreach(var rec in recs)
-            {
-                Console.WriteLine($"{ rec.Id}: { rec.FirstName} { rec.LastName}");
+            //var recs = db.LoadRecords<PersonModel>("Users");
+            //foreach(var rec in recs)
+            //{
+            //    Console.WriteLine($"{ rec.Id}: { rec.FirstName} { rec.LastName}");
 
-                if (rec.PrimaryAddress != null)
-                {
-                    Console.WriteLine(rec.PrimaryAddress.City);
-                }
+            //    if (rec.PrimaryAddress != null)
+            //    {
+            //        Console.WriteLine(rec.PrimaryAddress.City);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            var recs = db.LoadRecords<NameModel>("Users");
+            foreach (var rec in recs)
+            {
+                Console.WriteLine($"{ rec.Id}:{ rec.FirstName} { rec.LastName}");
                 Console.WriteLine();
             }
 
-            var oneRecord = db.LoadRecordById<PersonModel>("Users", new Guid("e90553db-1e84-46e8-b20e-03324b18bee9"));
-            Console.WriteLine($"{ oneRecord.Id}: { oneRecord.FirstName} { oneRecord.LastName}");
+
+            //var oneRecord = db.LoadRecordById<PersonModel>("Users", new Guid("e90553db-1e84-46e8-b20e-03324b18bee9"));
+            //Console.WriteLine($"{ oneRecord.Id}: { oneRecord.FirstName} { oneRecord.LastName}");
+            //oneRecord.DateOfBirthday = new DateTime(1987, 03, 18, 0, 0, 0, DateTimeKind.Utc);
+            //db.UpsertRecord("Users", oneRecord.Id, oneRecord);
+
+            //db.DeleteRecord<PersonModel>("Users", oneRecord.Id);
 
             Console.ReadLine();
         }
     }
+
+    public class NameModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
 
     public class PersonModel
     {
@@ -57,6 +79,8 @@ namespace MongoDbDemo
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public AddressModel PrimaryAddress { get; set; }
+        [BsonElement("dob")]
+        public DateTime DateOfBirthday { get; set; }
     }
 
     public class AddressModel
@@ -88,7 +112,7 @@ namespace MongoDbDemo
             collection.InsertOne(record);
         }
 
-        public List<T> LoadResource<T>(string table)
+        public List<T> LoadRecords<T>(string table)
         {
             var collection = db.GetCollection<T>(table);
             return collection.Find(new BsonDocument()).ToList();
@@ -102,6 +126,17 @@ namespace MongoDbDemo
             return collection.Find(filter).First();
         }
 
+        public void UpsertRecord<T>(string table, Guid id, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            var results = collection.ReplaceOne(new BsonDocument("_id", id), record, new UpdateOptions { IsUpsert = true});
+        }
 
+        public void DeleteRecord<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);
+        }
     }
 }
