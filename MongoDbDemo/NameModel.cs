580f1ef864ece59d24b7d7847ab0dcc17c03ae66
leandromtr <leandromtr@hotmail.com>
2021-04-13 21:06:40 +0100

    refatoring classes

---
+++ MongoDbDemo/NameModel.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace MongoDbDemo
+{
+    public class NameModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
