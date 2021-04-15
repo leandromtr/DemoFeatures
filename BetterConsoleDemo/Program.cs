a01cccfbb3d9ab8986da87001df8902c15950f2e
leandro <leandromtr@hotmail.com>
2021-04-16 00:30:54 +0100

    Add Builder setup: BuildConfig

--- BetterConsoleDemo/Program.cs
+++ BetterConsoleDemo/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
 
+
+// DI, Serilog, settings
 namespace BetterConsoleDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsetings.json.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
         }
     }
 }
