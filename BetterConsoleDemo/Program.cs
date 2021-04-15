f8fa936124b45f5a0dea16dcf17e45d01af9b3e6
leandro <leandromtr@hotmail.com>
2021-04-15 23:30:12 +0100

    Add BetterConsoleDemo

---
+++ BetterConsoleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BetterConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
