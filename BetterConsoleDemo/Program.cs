1fb23ec0836743f16a7c8c86910a5a424ccd0757
leandromtr <leandromtr@hotmail.com>
2021-04-17 18:02:24 +0100

    Refactoring and Configuration for Serilog in appsetings

--- BetterConsoleDemo/Program.cs
+++ BetterConsoleDemo/Program.cs
@@ -32,15 +32,18 @@ namespace BetterConsoleDemo
                     services.AddTransient<IGreetingService, GreetingService>();
                 })
                 .UseSerilog()
-                .Build(); ;
+                .Build();
+
+            var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+            svc.Run();
 
         }
 
         static void BuildConfig(IConfigurationBuilder builder)
         {
             builder.SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsetings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsetings.json.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.json.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
                 .AddEnvironmentVariables();
         }
     }
