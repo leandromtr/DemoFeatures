2614124ca1e92a406922be9680d8d4ce1a7d34e8
leandro <leandromtr@hotmail.com>
2021-05-10 09:30:39 +0100

    Add category server-side validation

--- MVCDemo/Controllers/CategoryController.cs
+++ MVCDemo/Controllers/CategoryController.cs
@@ -32,9 +32,14 @@ namespace MVCDemo.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
-            _db.SaveChanges();            
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
         }
     }
 }
