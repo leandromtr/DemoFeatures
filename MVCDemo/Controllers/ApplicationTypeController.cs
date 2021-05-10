0813a79a2464b73aaeddfc40bbfa51effd009ab6
leandromtr <leandromtr@hotmail.com>
2021-05-10 09:06:49 +0100

    add ApplicationType Controller

---
+++ MVCDemo/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Data;
+using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly MVCDemoDbContext _db;
+
+        public ApplicationTypeController(MVCDemoDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
+            return View(objList);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ApplicationType obj)
+        {
+            _db.ApplicationType.Add(obj);
+            _db.SaveChanges();            
+            return RedirectToAction("Index");
+        }
+    }
+}
