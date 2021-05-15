cfed9285587ee84a9c45a53f7b7e8bcbe64a257e
leandromtr <leandromtr@hotmail.com>
2021-05-15 19:01:17 +0100

    add crud to Application Type

--- MVCDemo/Controllers/ApplicationTypeController.cs
+++ MVCDemo/Controllers/ApplicationTypeController.cs
@@ -30,11 +30,71 @@ namespace MVCDemo.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(ApplicationType obj)
         {
-            _db.ApplicationType.Add(obj);
-            _db.SaveChanges();            
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+                return NotFound();
+
+            _db.ApplicationType.Remove(obj);
+            _db.SaveChanges();
             return RedirectToAction("Index");
+
+            return View(obj);
         }
     }
 }
--- MVCDemo/Models/ApplicationType.cs
+++ MVCDemo/Models/ApplicationType.cs
@@ -7,17 +7,11 @@ using System.Threading.Tasks;
 
 namespace MVCDemo.Models
 {
-    public class Category
+    public class ApplicationType
     {
         [Key]
         public int Id { get; set; }
-
         [Required]
         public string Name { get; set; }
-
-        [Required]
-        [DisplayName("Display Order")]
-        [Range(1, int.MaxValue, ErrorMessage ="Display order for category must be greater than 0")]
-        public int DisplayOrder { get; set; }
     }
 }
