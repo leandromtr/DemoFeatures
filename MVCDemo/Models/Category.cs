bc23560bb619b062b8c4691fd235bae425522114
leandro <leandromtr@hotmail.com>
2021-05-15 19:01:17 +0100

    add crud to Application Type

--- MVCDemo/Models/Category.cs
+++ MVCDemo/Models/Category.cs
@@ -7,10 +7,17 @@ using System.Threading.Tasks;
 
 namespace MVCDemo.Models
 {
-    public class ApplicationType
+    public class Category
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Display Order")]
+        [Range(1, int.MaxValue, ErrorMessage ="Display order for category must be greater than 0")]
+        public int DisplayOrder { get; set; }
     }
 }
