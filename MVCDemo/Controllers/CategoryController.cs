e31d2107087dd887a8835beb6fd5a88293bd57cc
leandro <leandromtr@hotmail.com>
2021-05-15 18:19:30 +0100

    add edit category page

--- MVCDemo/Controllers/CategoryController.cs
+++ MVCDemo/Controllers/CategoryController.cs
@@ -30,11 +30,39 @@ namespace MVCDemo.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
             if (ModelState.IsValid)
             {
                 _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id ==null || id == 0)
+                return NotFound();
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
