00c3d60de54b10fc48d8b9641b1043feceb58ab2
leandro <leandromtr@hotmail.com>
2021-05-20 22:00:17 +0100

    launch hangfire

---
+++ HangfireDemo/Controllers/HangfireController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangfireDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HangfireController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from hangfire webApi!");
+        }
+
+    }
+}
