311b012a2051f1b92f90edaa6eb57585cb723d59
leandromtr <leandromtr@hotmail.com>
2021-05-30 12:26:49 +0100

    Add Delay hangfire job

--- HangfireDemo/Controllers/HangfireController.cs
+++ HangfireDemo/Controllers/HangfireController.cs
@@ -12,12 +12,24 @@ namespace HangfireDemo.Controllers
     [Route("api/[controller]")]
     public class HangfireController : ControllerBase
     {
+        // Fire And Forget Job
         [HttpPost]
         [Route("[action]")]
         public IActionResult Welcome()
         {
             var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to this project"));
             return Ok($"Job Id: {jobId} Welcome email sent");
+        }
+
+
+        // Delay Job
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Discount()
+        {
+            int timeInSecounds = 10;
+            var jobId = BackgroundJob.Schedule(() => SendWelcomeEmail("Welcome to this project"), TimeSpan.FromSeconds(timeInSecounds));
+            return Ok($"Job Id: {jobId} Discount email will be send in {timeInSecounds} second(s)");
         }
 
         public void SendWelcomeEmail(string text)
