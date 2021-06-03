1bb566fd53839bd70d22e5e796046e39027bf15d
leandro <leandromtr@hotmail.com>
2021-06-03 23:49:11 +0100

    Add String TitleCase

--- StringDemo/Program.cs
+++ StringDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace StringDemo
 {
@@ -7,6 +8,27 @@ namespace StringDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            StringConversion();
         }
+
+        private static void StringConversion()
+        {
+            string testString = "This is the FBI calling!";
+            TextInfo currentTextInfo = CultureInfo.CurrentCulture.TextInfo;
+            TextInfo englishTextInfo = new CultureInfo("en-US", false).TextInfo;
+            string result;
+
+            result = testString.ToLower();
+            Console.WriteLine(result);
+
+            result = testString.ToUpper();
+            Console.WriteLine(result);
+
+            result = currentTextInfo.ToTitleCase(testString);
+            Console.WriteLine(result);
+
+            result = englishTextInfo.ToTitleCase(testString);
+            Console.WriteLine(result);
+        }         
     }
 }
