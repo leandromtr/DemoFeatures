6a06d2550536c8d0bfbd7425bbc4ecfc11718600
leandromtr <leandromtr@hotmail.com>
2021-08-08 14:11:52 +0200

    Add Generators class and limiting the yield return

--- YieldDemo/Program.cs
+++ YieldDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace YieldDemo
 {
@@ -23,6 +24,21 @@ namespace YieldDemo
             foreach (var p in people)
             {
                 Console.WriteLine($"Read {p.FirstName} {p.LastName}");
+            }
+
+            var primeNumbers = Generators.GetPrimeNumber().Take(10);
+            foreach(var prime in primeNumbers)
+            {
+                Console.WriteLine(prime);
+            }
+
+            Console.WriteLine("Waiting for user input");
+            Console.ReadLine();
+
+            primeNumbers = Generators.GetPrimeNumber().Take(15);
+            foreach (var prime in primeNumbers)
+            {
+                Console.WriteLine(prime);
             }
 
             Console.WriteLine("End of the App");
@@ -51,6 +67,38 @@ namespace YieldDemo
 
             return output;
         }
+    }
+
+
+    public class Generators
+    {
+        public static IEnumerable<int> GetPrimeNumber()
+        {
+            int counter = 1;
+            while (true)
+            {
+                if (IsPrimeNumber(counter))
+                {
+                    yield return counter;
+                }
+                counter++;
+            }
+        }
+
+        private static bool IsPrimeNumber(int value)
+        {
+            bool output = true;
+            for (int i = 2; i < value / 2; i++)
+            {
+                if (value % i == 0)
+                {
+                    output = false;
+                    break;
+                }
+            }
+            return output;
+        }
+
     }
 
 }
