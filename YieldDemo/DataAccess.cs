a8462d86b71526cdbb1fbbad5c3306e7c5dff2ea
LeandroReis <leandromtr@hotmail.com>
2021-08-08 14:42:36 +0200

    Refactoring Yield project

---
+++ YieldDemo/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace YieldDemo
+{
+    public class DataAccess
+    {
+        public static IEnumerable<PersonModel> GetPeople()
+        {
+            List<PersonModel> output = new();
+
+            output.Add(new PersonModel("Leandro", "Reis"));
+            output.Add(new PersonModel("Lucas", "Sousa"));
+            output.Add(new PersonModel("Teteus", "Reis"));
+
+            return output;
+        }
+    }
+
+}
