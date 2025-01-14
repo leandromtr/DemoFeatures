f1a3668be9958b5302cef74745630ec6a41d2ff2
Leandro Reis <leandro.reis@nextbitt.com>
2025-01-14 05:14:14 +0000

    feat: Add Throw library project

---
+++ ThrowLibrary/Program.cs
@@ -0,0 +1,73 @@
+﻿using HelperLibrary;
+using System.Text.RegularExpressions;
+using Throw;
+
+
+List<DateTime> dateList = new List<DateTime>()
+{
+    DateTime.Now,
+    DateTime.Now.AddDays(-984),
+    DateTime.Now.AddDays(34234),
+};
+
+
+List<int> ageList = new List<int>()
+{
+    34,
+    11,
+    143,
+    76,
+    18,
+    -2
+};
+
+List<string> nameList = new List<string>()
+{
+    "Leandro Reis",
+    "Lucas Silva",
+    "Lessa de Oliveira",
+    "Ana Sa",
+    "Mateus Reis",
+    "Raxptos Xavier",
+    "Ana Sa",
+    ""
+};
+
+ThrowMethods helpers = new();
+
+//Name Validation
+Console.WriteLine("");
+Console.WriteLine("Example Name Validations");
+Console.WriteLine("--------------------------------------------------------------------------------------------");
+Console.WriteLine("Name                 - Error");
+Console.WriteLine("--------------------------------------------------------------------------------------------");
+foreach (var item in nameList)
+{
+    Console.WriteLine(string.Format("{0} - {1}", item.PadRight(20, ' '), helpers.NameValidation(item)));
+}
+
+
+//Numbers Validation
+Console.WriteLine("");
+Console.WriteLine("Example Numbers Validations");
+Console.WriteLine("--------------------------------------------------------------------------------------------");
+Console.WriteLine("Number               - Error");
+Console.WriteLine("--------------------------------------------------------------------------------------------");
+foreach (var item in ageList)
+{
+    Console.WriteLine(string.Format("{0} - {1}", item.ToString().PadRight(20, ' '), helpers.AdultAgeValidation(item)));
+}
+
+
+//DateTime Validation
+Console.WriteLine("");
+Console.WriteLine("Example DateTime Validations");
+Console.WriteLine("--------------------------------------------------------------------------------------------");
+Console.WriteLine("Date                 - Error");
+Console.WriteLine("--------------------------------------------------------------------------------------------");
+foreach (var item in dateList)
+{
+    Console.WriteLine(string.Format("{0} - {1}", item.ToString().PadRight(20, ' '), helpers.DateValidation(item)));
+}
+Console.ReadLine();
+
---
+++ ThrowLibrary/ThrowMethod.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Throw;
+
+namespace HelperLibrary;
+
+public class ThrowMethods
+{
+    public string NameValidation(string name)
+    {
+        try
+        {
+            name.Throw()
+                .IfEmpty()
+                .IfWhiteSpace()
+                .IfEqualsIgnoreCase("Leandro Reis")
+                .IfShorterThan(7)
+                .IfLongerThan(12)
+                .IfContains("xpto");
+            return "Ok";
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+
+
+    public string AdultAgeValidation(int age)
+    {
+        try
+        {
+            age.Throw()
+            .IfNegative()
+            .IfLessThan(18)
+            .IfGreaterThan(130);
+            return "Ok";
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+
+    public string DateValidation(DateTime date)
+    {
+        try
+        {
+            date.ThrowIfNull()
+                .IfGreaterThan(p => p.Date, DateTime.Now.AddDays(1));
+            return "Ok";
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+}
+
