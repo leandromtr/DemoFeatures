41cc8ce6ca33d5bb4851d26197543e41fce588b6
Leandro Reis <leandro.reis@nextbitt.com>
2025-01-14 05:26:04 +0000

    feat: Add Benchmark library

---
+++ Benchmarker/Program.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+namespace aaaa
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = BenchmarkRunner.Run<Demo>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net472, baseline:true)]
+    [SimpleJob(RuntimeMoniker.Net60)]
+    public class Demo
+    {
+        [Benchmark(Baseline = true)]
+        public string GetFullStringNormally()
+        {
+            string output = "";
+
+            for (int i = 0; i < 100; i++)
+            {
+                output += i;
+            }
+            return output;
+        }
+
+        [Benchmark]
+        public string GetFullStringWithStringBuilder()
+        {
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                output.Append(i);
+            }
+            return output.ToString();
+        }
+    }
+
+}
