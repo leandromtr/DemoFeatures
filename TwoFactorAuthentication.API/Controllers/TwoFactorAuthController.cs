3b7367947ccd90a503e633e3e7f1dcffd2a85dd6
Leandro Reis <leandro.reis@nextbitt.com>
2025-01-16 01:40:02 +0000

    feat: Aff Two FactorAuthentication project

---
+++ TwoFactorAuthentication.API/Controllers/TwoFactorAuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwoFactorAuthentication.API.Services;
+
+namespace TwoFactorAuthentication.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwoFactorAuthController : Controller
+    {
+        private readonly TwoFactorAuthService _twoFactorAuthService;
+
+        public TwoFactorAuthController(TwoFactorAuthService twoFactorAuthService)
+        {
+            _twoFactorAuthService = twoFactorAuthService;
+        }
+
+        [HttpGet("Generate")]
+        public IActionResult Index(string username)
+        {
+            var secretKey = _twoFactorAuthService.GenerateSecretKey();
+            var issuer = "TwoFactorAuthenticationApp";
+            var qrCodeUri = _twoFactorAuthService.GenerateQRCodeUri(issuer, username, secretKey);
+            var qrCodeImage = _twoFactorAuthService.GenerateQRCode(qrCodeUri);
+
+            return Ok(new
+            {
+                secretKey = secretKey,
+                qrCodeImage = qrCodeImage
+            });
+        }
+
+        [HttpPost("Validade")]
+        public IActionResult Validade(string code, string secretKey)
+        {
+            var isValid = _twoFactorAuthService.ValidateCode(secretKey, code);
+            if (isValid)
+            {
+                return Ok("Código válido.");
+            }
+
+            return Unauthorized("Código inválido.");
+        }
+    }
+}
---
+++ TwoFactorAuthentication.API/Services/TwoFactorAuthService.cs
@@ -0,0 +1,37 @@
+﻿using OtpNet;
+using QRCoder;
+
+namespace TwoFactorAuthentication.API.Services
+{
+    public class TwoFactorAuthService
+    {
+        public string GenerateSecretKey()
+        {
+            var secretKey = KeyGeneration.GenerateRandomKey(20);
+            return Base32Encoding.ToString(secretKey);
+
+        }
+
+        public string GenerateQRCodeUri(string issuer, string username, string secretKey)
+        {
+            return $"otpaulth://totp/{issuer}:{username}?secret={secretKey}&issuer={issuer}";
+        }
+
+        public string GenerateQRCode(string uri)
+        {
+            using (var qrGenerator = new QRCodeGenerator())
+            using (var qrCodeData = qrGenerator.CreateQrCode(uri, QRCodeGenerator.ECCLevel.Q))
+            using (var qrCode = new PngByteQRCode(qrCodeData))
+            {
+                byte[] qrCodeImage = qrCode.GetGraphic(20);
+                return Convert.ToBase64String(qrCodeImage);
+            };
+        }
+
+        public bool ValidateCode(string secretKey, string code)
+        {
+            var totp = new Totp(Base32Encoding.ToBytes(secretKey));
+            return totp.VerifyTotp(code, out _);
+        }
+    }
+}
